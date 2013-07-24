b979432d5b9f80c0fac8111569d10defa6d27e1d
Gina Turco <gturco88@gmail.com>
2013-07-24 11:44:29 -0700

    formated better chipseq

--- ChipSeq.sh
+++ ChipSeq.sh
@@ -6,22 +6,22 @@ start with image "Ubuntu 12.04 Xfce GUI v2"
 
 #load all bioinformatics pakages:
 
-sh RNASeq.sh
+  sh RNASeq.sh
 
 # SICER:
 
-wget http://home.gwu.edu/~wpeng/SICER_V1.1.tgz
-tar -xvzf SICER_V1.1.tgz
-cd SICER_V1.1
-vim SICER-df-rb.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
-vim SICER-df.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
-vim SICER-rb.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
-vim SICER.sh #change oathto SICER=/usr/local/bin/SICER1.1/SICER
+  wget http://home.gwu.edu/~wpeng/SICER_V1.1.tgz
+  tar -xvzf SICER_V1.1.tgz
+  cd SICER_V1.1
+  vim SICER-df-rb.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
+  vim SICER-df.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
+  vim SICER-rb.sh # change pathto SICER=/usr/local/bin/SICER1.1/SICER
+  vim SICER.sh #change oathto SICER=/usr/local/bin/SICER1.1/SICER
 
 # MACS:
 
-wget https://github.com/downloads/taoliu/MACS/MACS-1.4.2-1.tar.gz
-tar -xvzf MACS-1.4.2-1.tar.gz
-cd MACS-1.4.2/
-sudo python setup.py install
+  wget https://github.com/downloads/taoliu/MACS/MACS-1.4.2-1.tar.gz
+  tar -xvzf MACS-1.4.2-1.tar.gz
+  cd MACS-1.4.2/
+  sudo python setup.py install
 
