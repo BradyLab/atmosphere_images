19775bf3cef57add0382e51431ad6a37758af85d
Gina Turco <gturco88@gmail.com>
2013-07-25 13:08:26 -0700

    added more code for downloading needs for chipseq

--- ChipSeq.sh
+++ ChipSeq.sh
@@ -8,6 +8,10 @@ start with image "Ubuntu 12.04 Xfce GUI v2"
 
   sh RNASeq.sh
 
+# Scipy:
+
+  sudo pip install scipy
+
 # SICER:
 
   wget http://home.gwu.edu/~wpeng/SICER_V1.1.tgz
@@ -24,4 +28,12 @@ start with image "Ubuntu 12.04 Xfce GUI v2"
   tar -xvzf MACS-1.4.2-1.tar.gz
   cd MACS-1.4.2/
   sudo python setup.py install
+
+# gawk and sam2bed:
+  
+  sudo apt-get install gawk
+  http://bedops.googlecode.com/files/bedops_linux_x86_64-v2.2.0.tar.bz2
+  tar -xjf bedops_linux_x86_64-v2.2.0.tar.bz2
+  sudo mv bin/sam2bed /usr/local/bin/
+  sudo mv bin/sort-bed /usr/local/bin/
 
